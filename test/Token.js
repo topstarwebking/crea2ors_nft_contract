27f138d74491259dc1f963569257deaec4213899
snowflake-dev918 <lo.egan918@gmail.com>
2022-07-11 18:34:21 +0900

    v_3.3 added

--- test/Token.js
+++ test/Token.js
@@ -50,6 +50,41 @@ describe("Token contract", function () {
     expect(await hardhatToken.getCurMintedSupply(0)).to.equal(100);
     expect(await hardhatToken.uri(0)).to.equal("HHH");
     expect(await hardhatToken.contractURI()).to.equal("https://github.com");
+
+    await (
+      await hardhatToken.redeem(addr1.address, voucher, {
+        value: ethers.utils.parseEther("0.05"),
+      })
+    ).wait();
+
+    expect(await hardhatToken._currentTokenID()).to.equal(1);
+    expect(await hardhatToken.getCurMintedSupply(0)).to.equal(200);
+    expect(await hardhatToken.balanceOf(addr1.address, 0)).to.equal(200);
+
+    await hardhatToken.connect(addr1).setApprovalForAll(owner.address, true);
+    await (
+      await hardhatToken.transferNFT(0, 50, addr1.address, addr2.address)
+    ).wait();
+    expect(await hardhatToken.balanceOf(addr2.address, 0)).to.equal(50);
+
+    const voucher1 = {
+      tokenId: 1,
+      metaUri: "HHH",
+      mintCount: 200,
+      minPrice: 1,
+      initialSupply: 1000,
+      royaltyFee: 25,
+      royaltyAddress: owner.address,
+    };
+
+    await (
+      await hardhatToken.redeem(addr1.address, voucher1, {
+        value: ethers.utils.parseEther("0.05"),
+      })
+    ).wait();
+
+    expect(await hardhatToken._currentTokenID()).to.equal(2);
+    expect(await hardhatToken.getCurMintedSupply(1)).to.equal(200);
   });
 
   it("This is second mint", async function () {
