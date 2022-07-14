b846e3cdd911d54bb666bd85093cd4754f59775b
snowflake-dev918 <lo.egan918@gmail.com>
2022-07-14 10:36:15 +0800

    fee processing function added

--- contracts/Crea2ors_Manager.sol
+++ contracts/Crea2ors_Manager.sol
@@ -3,18 +3,28 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 interface ICrea2orsNFT {
-  function transferNFT(uint256 id, uint256 amount, address from, address to) external;
+  function transferNFT(uint256 id, uint256 amount, address from, address to, uint256 fund) external;
+}
+
+interface ICrea2Crypto {
+  function approve(address spender, uint256 fund) external;
 }
 
 contract Crea2ors_Manager {
   mapping(address => ICrea2orsNFT) collections;
+  ICrea2Crypto cr2Contract;
+
+  constructor(address cr2ContractAddress) {
+    cr2Contract = ICrea2Crypto(cr2ContractAddress);
+  }
 
   function addCollection(address newAddress) public {
     ICrea2orsNFT nftContract = ICrea2orsNFT(newAddress);
     collections[newAddress] = nftContract;
   }
 
-  function transferNFT(address collectionAddress, address from, address to, uint256 id, uint256 amount) public {
-    collections[collectionAddress].transferNFT(id, amount, from, to);
+  function transferNFT(address collectionAddress, address from, address to, uint256 id, uint256 amount, uint256 fund) public {
+    cr2Contract.approve(address(this), fund);
+    collections[collectionAddress].transferNFT(id, amount, from, to, fund);
   }
 }
